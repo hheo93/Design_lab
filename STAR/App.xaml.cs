f3dd7d7ac4111074e4872b821bbea8b813f02660
Eman Shah <eshah@albany.edu>
2019-10-12 23:20:01 -0400

    initial commit

---
+++ STAR/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace STAR
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new FrontPage())
+            {
+                BarBackgroundColor = Color.FromHex("#00009a")
+            };
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ STAR/FrontPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace STAR
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FrontPage : ContentPage
+    {
+        public FrontPage()
+        {
+            InitializeComponent();
+            NavigateCommand = new Command<Type>(async (Type pageType) =>
+            {
+                Page page = (Page)Activator.CreateInstance(pageType);
+                await Navigation.PushAsync(page);
+            });
+
+            BindingContext = this;
+        }
+
+        public ICommand NavigateCommand { private set; get; }
+    }
+}
