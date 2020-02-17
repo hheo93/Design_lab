aa9c5c389f609c5bb675722b6fab0d4d50efadd5
Eman Shah <eshah@albany.edu>
2020-02-17 12:20:28 -0500

    added connection string to newtrip

---
+++ STAR/HTTPClientInstance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Net.Http;
+using System.Data.SqlClient;
+
+namespace STAR.Utilities
+{
+    public class HTTPClientInstance
+    {
+        public static readonly HttpClient client;
+        
+        static HTTPClientInstance()
+        {
+            client = new HttpClient();
+        }
+    }
+}
--- STAR/NewTripPage.xaml.cs
+++ STAR/NewTripPage.xaml.cs
@@ -4,15 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Net.Http;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using STAR.Utilities;
 
 namespace STAR
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class NewTripPage : ContentPage
     {
-
+        Dictionary<string, string> TableInfo = new Dictionary<string, string>();
+        
         public NewTripPage()
         {
             InitializeComponent();
@@ -30,9 +33,26 @@ namespace STAR
                     + Environment.NewLine + Environment.NewLine
                     + "Return: " + ReturnAddress.Text
                     + Environment.NewLine + Environment.NewLine
-                    + "Additional Instructions: " + Comments.Text;
+                    + "Additional Instructions: " + YesPCA.ToString();
 
             bool answer = await DisplayAlert("Verify Information:", message, "Confirm", "Cancel");
+
+            if (answer)
+            {
+                TableInfo.Add("PUAddress", CurrAddress.Text);
+                TableInfo.Add("PUTime", temp);
+                TableInfo.Add("DOAddress", DestAddress.Text);
+                TableInfo.Add("RAddress", ReturnAddress.Text);
+                TableInfo.Add("RTime", ReturnDatePicked.Date.ToShortDateString());
+                TableInfo.Add("PCA", YesPCA.ToString());
+                TableInfo.Add("Payment", "Ticket");
+                TableInfo.Add("Comments", Comments.Text);
+
+                var content = new FormUrlEncodedContent(TableInfo);
+                var response = await HTTPClientInstance.client.PostAsync("https://paratransitservices.azurewebsites.net/newtrip", content);
+                var responseString = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responseString);
+            }
         }
     }
 }
